856e6a769053f8a68c451e643213528a21590bfb
tatuaua <tatu.niemikorpi@gmail.com>
2025-03-07 14:03:02 +0200

    refactor endpoint

--- src/main/kotlin/com/example/infone/controller/DataPointController.kt
+++ src/main/kotlin/com/example/infone/controller/DataPointController.kt
@@ -4,28 +4,34 @@ import com.example.infone.model.DataPoint
 import com.example.infone.repository.DataPointRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.http.HttpResponse
 
 @RestController
+@RequestMapping("/api")
 class DataPointController(private val dataPointRepository: DataPointRepository) {
 
+    @Value("\${api.key}")
+    private lateinit var apiKey: String
     private val logger: Logger = LoggerFactory.getLogger(DataPointController::class.java)
 
-    @GetMapping("/datapoints")
-    fun getDataPoints(): List<DataPoint> {
-        logger.info("Fetching all data points")
-        val dataPoints = dataPointRepository.getDataPoints()
-        logger.info("Retrieved {} data points", dataPoints.size)
-        return dataPoints
-    }
-
     @GetMapping("/datapoints/{ids}")
-    fun getDataPoints(@PathVariable ids: List<Int>): List<DataPoint> {
-        logger.info("Fetching data points with ids: {}", ids)
-        val dataPoints = dataPointRepository.getDataPoints(ids)
+    fun getDataPoints(@RequestHeader("X-API-KEY") apiKey: String, @PathVariable ids: List<Int>): ResponseEntity<List<DataPoint>> {
+        if (apiKey != this.apiKey) {
+            logger.warn("Invalid API key: {}", apiKey)
+            return ResponseEntity.status(401).build()
+        }
+        val dataPoints: List<DataPoint>
+        if(ids.isEmpty()) {
+            logger.info("Fetching all data points")
+            dataPoints = dataPointRepository.getDataPoints()
+        } else {
+            logger.info("Fetching data points by ids: {}", ids)
+            dataPoints = dataPointRepository.getDataPoints(ids)
+        }
         logger.info("Retrieved {} data points", dataPoints.size)
-        return dataPoints
+        return ResponseEntity.ok(dataPoints)
     }
 }
