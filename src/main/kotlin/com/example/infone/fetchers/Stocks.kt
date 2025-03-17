91c7c58ddb28e88925376acdecc7a97899187cb8
tatuaua <tatu.niemikorpi@gmail.com>
2025-03-17 20:57:30 +0200

    add more stocks

--- src/main/kotlin/com/example/infone/fetchers/Stocks.kt
+++ src/main/kotlin/com/example/infone/fetchers/Stocks.kt
@@ -7,3 +7,15 @@ class SP500 : StockIndexFetcher("^GSPC", "S&P 500", "S&P 500 index at close")
 
 @Component
 class STOXX50E : StockIndexFetcher("^STOXX50E", "STOXX 50 Europe", "STOXX 50 Europe index at close")
+
+@Component
+class DowJones : StockIndexFetcher("^DJI", "Dow Jones Indus. Avg", "Dow Jones Industrial Average at close")
+
+@Component
+class Nasdaq : StockIndexFetcher("^IXIC", "NASDAQ Composite", "NASDAQ Composite index at close")
+
+@Component
+class FTSE100 : StockIndexFetcher("^FTSE", "FTSE 100", "FTSE 100 index at close")
+
+@Component
+class Nikkei225 : StockIndexFetcher("^N225", "Nikkei 225", "Nikkei 225 index at close")
