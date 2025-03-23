6e4a87e4729ff8f419aa3b6da5e9795d0470f367
tatuaua <tatu.niemikorpi@gmail.com>
2025-03-23 19:12:32 +0200

    simplify fetcher loop

--- src/main/kotlin/com/example/infone/InfoneApplication.kt
+++ src/main/kotlin/com/example/infone/InfoneApplication.kt
@@ -3,7 +3,9 @@ package com.example.infone
 import com.example.infone.utils.FetcherRunner
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -14,10 +16,12 @@ fun main(args: Array<String>) {
     val context = runApplication<InfoneApplication>(*args)
     val fetcherRunner = context.getBean(FetcherRunner::class.java)
 
-    while (true) {
-        CoroutineScope(Dispatchers.Default).launch {
-            fetcherRunner.runAsync()
+    runBlocking {
+        while (true) {
+            launch {
+                fetcherRunner.runAsync()
+            }
+            delay(600000)
         }
-        Thread.sleep(600000)
     }
 }
--- src/main/kotlin/com/example/infone/utils/Util.kt
+++ src/main/kotlin/com/example/infone/utils/Util.kt
@@ -1,10 +1,21 @@
 package com.example.infone.utils
 
+import com.example.infone.model.DataPoint
+import com.example.infone.model.DataPointFetcher
 import java.text.DecimalFormat
 
 object Util {
 
     fun formatDouble(value: Double): String {
         return DecimalFormat("#.###").format(value).replace(",", ".")
+    }
+
+    fun mockFetcherWithRandomDelay(n: Int) : DataPointFetcher {
+        return object : DataPointFetcher {
+            override fun fetch(): DataPoint {
+                Thread.sleep((Math.random() * 1000).toLong())
+                return DataPoint(n, "mock $n", "mock $n", "mock $n")
+            }
+        }
     }
 }
